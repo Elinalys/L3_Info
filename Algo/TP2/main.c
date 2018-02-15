49896624b249001f81b01d26c28b7604953ecf22
Loris <loriscrc@gmail.com>
2018-02-15 18:59:52 +0100

    rien ne va plus

--- Algo/TP2/main.c
+++ Algo/TP2/main.c
@@ -6,19 +6,61 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
+
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define RESET   "\033[0m"
+#define BOLD  "\033[1m"
+
+void affichage(int **D, int n, int m) {
+	printf(BOLD "n = " RESET "%d\n",n);
+	printf(BOLD "m = " RESET "%d\n",m);
+	printf("\n");
+
+	printf(BOLD "Tableau des substitutions :\n\n" RESET);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("%d\t", D[i][j]);
+		}
+		printf("\n\n");
+	}
+}
+
+// retourne la souchaine de 0 à len
+char *substr(char *chaineSource, int len) { 
+  char *dest = NULL;                        
+  if (len > 0) {                          
+    dest = (char *)calloc(len+1, 1);      
+    if (NULL != dest) {
+      strncat(dest,chaineSource,len);            
+    }
+  }                                       
+  return dest;                            
+}
+
+// ça marche pas lalalala
+int distanceR(char *A, char *B) {
+	int n = strlen(A);
+	int m = strlen(B);
+
+	if (n == 0) return n;
+	if (m == 0) return m;
+	else {
+		if (A[n] == B[m]) return distanceR(substr(A, n-1), substr(B, m-1));
+		else {
+			return 1 + MIN(distanceR(substr(A, n-1), substr(B, m-1)), MIN(distanceR(A, substr(B, m-1)), distanceR(substr(A, n-1), B)));
+		}
+	}
+}
 
 int distance(char *A, char *B) {
 
 	int n = strlen(A)+1;
 	int m = strlen(B)+1;
 
-	printf("n = %d\n",n);
-	printf("m = %d\n",m);
-
 	// int D[n][m];
 	int **D = (int **)malloc(n*sizeof(int*));
-	for (int i = 0; i<n; i++) {
+	for (int i = 0; i < n; i++) {
 		D[i] = (int *)malloc(m*sizeof(int));
 	}
 
@@ -34,12 +76,8 @@ int distance(char *A, char *B) {
 			}
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			printf("%d\t", D[i][j]);
-		}
-		printf("\n\n");
-	}
+
+	affichage(D, n, m);
 
 	for (int i = 0; i < n; i++) free(D[i]);
 	free(D);
@@ -50,9 +88,12 @@ int distance(char *A, char *B) {
 int main(int argc, char const *argv[]) {
 	char *A = "tourte";
 	char *B = "tartre";
+	printf("%ld\n", strlen(A));
+	char *C = substr(A, 5);
+	printf("%s\n", C);
 
 	// distance(A, B);
-	printf("distance = %d\n",distance(A, B));
+	// printf(BOLD "distance %s et %s" RESET " = %d\n",A, B, distanceR(A, B));
 	// printf("%d\n",MIN(3,7));
 
 	return 0;
