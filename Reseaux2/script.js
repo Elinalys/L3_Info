b505d22bd61f7667140703b929bf7239e09800f7
Loris <loriscrc@gmail.com>
2018-02-21 17:39:50 +0100

    jsr

--- Reseaux2/script.js
+++ Reseaux2/script.js
@@ -12,18 +12,30 @@ var draw = function() {
   var canvas = document.getElementById('tutorial');
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
-
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(10, 10, 50, 50);
-
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect(30, 30, 50, 50);
+    // ctx.fillStyle = 'rgb(200, 0, 0)';
+    // ctx.fillRect(10, 10, 50, 50);
+    // ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    // ctx.fillRect(30, 30, 50, 50);
   	ctx.font = '48px monospace';
-  	ctx.fillText('Hello world', 50, 50);
+    msg = input.value;
+  	ctx.fillText(msg, 50, 50);
   }
 }
 
-// document.write("test");
-//window.addEventListener("load", draw);
+var clear = function() {
+  var canvas = document.getElementById('tutorial');
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (input.value !== '') {
+      input.value = '';
+    }
+  }
+
+}
+
+// window.addEventListener("load", draw);
+button = document.getElementById("delete");
+button.addEventListener("click", clear);
 input = document.getElementById("chaine");
-input.addEventListener("input", draw)
+input.addEventListener("input", draw);
