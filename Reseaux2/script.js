9d1df2c92dbff19fe54e35dec8fbafa2135ef99e
Loris <loriscrc@gmail.com>
2018-02-24 13:38:32 +0100

    update

--- Reseaux2/script.js
+++ Reseaux2/script.js
@@ -12,10 +12,10 @@ var draw = function() {
   var canvas = document.getElementById('tutorial');
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
-    // ctx.fillStyle = 'rgb(200, 0, 0)';
-    // ctx.fillRect(10, 10, 50, 50);
-    // ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    // ctx.fillRect(30, 30, 50, 50);
+    ctx.fillStyle = 'rgb(200, 0, 0)';
+    ctx.fillRect(10, 10, 50, 50);
+    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    ctx.fillRect(30, 30, 50, 50);
   	ctx.font = '48px monospace';
     msg = input.value;
   	ctx.fillText(msg, 50, 50);
